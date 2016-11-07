32b972f80247737d4d39128a6a6924ed2997d7fd
Thachapong Chirarattanawong <thachapong.chi@ascendcorp.com>
2016-11-07 14:38:27 +0700

    Change to send data as object.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var port = 8081;
 var server = app.listen(port, function() {
     console.log('Listening on port: ' + port);
 });
+var io = require('socket.io').listen(server);
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -15,34 +17,9 @@ app.get('/', function(req, res) {
     res.render('index');
 });
 
-
-var io = require('socket.io').listen(server);
-
-// เมื่อมี client เข้ามาเชื่อมต่อให้ทำอะไร?
 io.on('connection', function(socket) {
-    // เมื่อได้รับข้อมูลจากท่อ "chat" ให้ทำอะไร?
-    socket.on('chat', function(message) {
-      // ส่งข้อความที่ได้ไปหาทุกๆ client ที่เชื่อมต่อกับท่อชื่อ "chat"
-      io.emit('chat', message + ' @' + getTime());
-      console.log('message: ' + message);
+    socket.on('chat', function(data) {
+        io.emit('chat', data);
+        console.log('Message: ' + data.message);
     });
 });
-
-
-
-function getTime() {
-
-    var date = new Date();
-
-    var hour = date.getHours();
-    hour = (hour < 10 ? "0" : "") + hour;
-
-    var min  = date.getMinutes();
-    min = (min < 10 ? "0" : "") + min;
-
-    var sec  = date.getSeconds();
-    sec = (sec < 10 ? "0" : "") + sec;
-
-    return hour + ":" + min + ":" + sec;
-
-}
